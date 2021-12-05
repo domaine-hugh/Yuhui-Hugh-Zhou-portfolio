b43e68a390e02e52f6b732c792b14738631044b5
YUHUI ZHOU <hughzhoutrt@gmail.com>
2021-12-05 16:43:57 -0500

    videos are under control now

--- scripts.js
+++ scripts.js
@@ -1,36 +1,45 @@
-const app = {
-    props: {
+const app = {}
 
-    },
-    ultils: {
-        listenToRange: function (target, output) {
-            document.querySelector(target).addEventListener('change', function (e) {
-                console.log(e.target.value);
-                output = e.target.value
+//Display project videos
+app.hoverDisplayVideo = () => {
+    const video = document.querySelectorAll('.projectVideos');
+    for( let i = 0 ; i < video.length ; i++) {
+        ['mouseenter', 'focusin', 'touchstart'].forEach((e) => {
+            video[i].addEventListener(e, function() {
+                video[i].play();
             })
-        }
+        })
+    }
+    for (let i = 0; i < video.length ; i++) {
+        ['mouseleave', 'focusout', 'touchmove'].forEach((e) => {
+            video[i].addEventListener(e, function() {
+                video[i].pause();
+            })
+        })
     }
 }
 
 
 app.init = function () {
-console.log('start!');
-//About Section switch function
-document.querySelector("#aboutSectionRange").addEventListener('change', function (e) {
-    if (e.target.value === "1") {
-        document.querySelector("#profile").style.display = 'none';
-        document.querySelector("#education").style.display = 'block';
-        document.querySelector("#plumAssessment").style.display = 'none';
-    } else if (e.target.value === "2") {
-        document.querySelector("#profile").style.display = 'none';
-        document.querySelector("#education").style.display = 'none';
-        document.querySelector("#plumAssessment").style.display = 'block';
-    } else {
-        document.querySelector("#profile").style.display = 'block';
-        document.querySelector("#education").style.display = 'none';
-        document.querySelector("#plumAssessment").style.display = 'none';
-    }
-})
+    //About Section switch function
+    document.querySelector("#aboutSectionRange").addEventListener('change', function (e) {
+        if (e.target.value === "1") {
+            document.querySelector("#profile").style.display = 'none';
+            document.querySelector("#education").style.display = 'block';
+            document.querySelector("#plumAssessment").style.display = 'none';
+        } else if (e.target.value === "2") {
+            document.querySelector("#profile").style.display = 'none';
+            document.querySelector("#education").style.display = 'none';
+            document.querySelector("#plumAssessment").style.display = 'block';
+        } else {
+            document.querySelector("#profile").style.display = 'block';
+            document.querySelector("#education").style.display = 'none';
+            document.querySelector("#plumAssessment").style.display = 'none';
+        }
+    })
+
+    //Calling display project videos
+    app.hoverDisplayVideo();
 
 }
 
